9221f03906f17f9a63227682ebf57acf99590b20
Zander Keith <zkeith@wisc.edu>
2020-04-19 14:30:01 -0500

    Began creating GUI as laid out in Design google doc

--- MilkWeight/application/Main.java
+++ MilkWeight/application/Main.java
@@ -26,6 +26,7 @@ import java.util.List;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -36,6 +37,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -46,44 +48,93 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 	// store any command-line arguments that were entered.
-		// NOTE: this.getParameters().getRaw() will get these also
-		private List<String> args;
+	// NOTE: this.getParameters().getRaw() will get these also
+	private List<String> args;
 
-		private static final int WINDOW_WIDTH = 300;
-		private static final int WINDOW_HEIGHT = 200;
-		private static final String APP_TITLE = "MilkWeight";
+	private static final int WINDOW_WIDTH = 800;
+	private static final int WINDOW_HEIGHT = 640;
+	private static final String APP_TITLE = "MilkWeight";
+	
+	
+	private void homeButtonAction() {
+		System.out.println("Home Button Pressed");
+	}
+	
+	private void importFileButtonAction() {
+		System.out.println("Import File Button Pressed");
+	}
+	
+	private void exportAsCSVButtonAction() {
+		System.out.println("Export as CSV File Button Pressed");
+	}
+	
+	private void submitButtonAction() {
+		System.out.println("Submit Button Pressed");
+	}
+	
+	/**
+	 * Sets up all GUI elements.
+	 */
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// Labels
+		Label fileLabel = new Label("File");
+		Label reportLabel = new Label("Report");
+		Label farmIDLabel = new Label("Farm ID");
+		Label yearLabel = new Label("Year(Enter \"all\" for all available data)");
+		Label farmReportLabel = new Label("Farm Report");
+		TextField farmIDField = new TextField("");
+		TextField yearField = new TextField("");
+		
+		// Buttons
+		Button homeButton = new Button("Home");
+		homeButton.setOnAction(e -> homeButtonAction());
+		
+		Button importFileButton = new Button("Import FIle");
+		importFileButton.setOnAction(e -> importFileButtonAction());
+		
+		Button exportAsCSVButton = new Button("Export as CSV");
+		exportAsCSVButton.setOnAction(e -> exportAsCSVButtonAction());
+		
+		Button submitButton = new Button("Submit");
+		submitButton.setOnAction(e -> submitButtonAction());
+		
+		// Create left panel of buttons
+		VBox leftOptionPanel = new VBox();
+		leftOptionPanel.setSpacing(20); //TODO no magic numbers in spacing. define constants elsewhere
+		leftOptionPanel.setPadding(new Insets(15, 15, 15, 15)); //TODO verify insets are allowed (javafx.geometry)
+		
+		VBox fileOptionGroup = new VBox();
+		fileOptionGroup.getChildren().addAll(fileLabel, importFileButton, exportAsCSVButton);
+		
+		VBox reportOptionGroup = new VBox(farmReportLabel);
+		
+		leftOptionPanel.getChildren().addAll(homeButton, fileOptionGroup, reportOptionGroup);
+		
+		// 
+		
+		// Main layout is Border Pane example (top,left,center,right,bottom)
+		BorderPane root = new BorderPane();
 
-		@Override
-		public void start(Stage primaryStage) throws Exception {
-			// Labels
-			Label topLabel = new Label(APP_TITLE);
-			TextField copyLabel = new TextField("www.youtube.com/watch?v=dQw4w9WgXcQ");
-			copyLabel.setEditable(false);
-			
-			
-			// Main layout is Border Pane example (top,left,center,right,bottom)
-			BorderPane root = new BorderPane();
-			
-			// Set contents in border pane
-			root.setTop(topLabel);
-			root.setCenter(copyLabel);
-			
-			
-			Scene mainScene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+		// Set contents in border pane
+		root.setLeft(leftOptionPanel);
 
-			// Add the stuff and set the primary stage
-			primaryStage.setTitle(APP_TITLE);
-			primaryStage.setScene(mainScene);
-			primaryStage.show();
-		}
+		Scene mainScene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+		// Add the stuff and set the primary stage
+		primaryStage.setTitle(APP_TITLE);
+		primaryStage.setScene(mainScene);
+		primaryStage.show();
+	}
+
 
 	/**
+	 * TODO method header needed?
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		launch(args);
-
 	}
 
 }
